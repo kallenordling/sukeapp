4a8d25c4f3cd7bf0902d4c37e42443890007af42
kallenordling <57295078+kallenordling@users.noreply.github.com>
2025-06-03 10:34:39 +0300

    Update __init__.py

--- suke/__init__.py
+++ suke/__init__.py
@@ -12,8 +12,7 @@ user=os.environ.get("user")
 password=os.environ.get("password")
 host=os.environ.get("host")
 database=os.environ.get("database")
-DATABASE_URL = "postgres://"+user+":"+password+"@"+host+"/"+database
-print(DATABASE_URL)
+DATABASE_URL = "postgresql://"+user+":"+password+"@"+host+"/"+database
 app = Flask(__name__, template_folder='./templates')
 UPLOAD_FOLDER = './uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
